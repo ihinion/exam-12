ab4e995c95f1f316894a5bc1fd8c81b1ae132e70
ilia <jamiashvili@gmail.com>
2021-01-23 10:40:44 +0600

    Добавлена модель Friend

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from webapp.models import Friend
 
-# Register your models here.
+
+admin.site.register(Friend)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Friend(models.Model):
+    user = models.ForeignKey(get_user_model(), verbose_name='User', related_name='friends',
+                             on_delete=models.CASCADE)
+    target_user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,
+                                    verbose_name='Friend')
